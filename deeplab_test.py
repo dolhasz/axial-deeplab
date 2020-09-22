96399f53c637eb153c8d9a624d9f65b56f68e804
Alan <alan.dolhasz@gmail.com>
2020-09-22 22:24:32 +0100

    FIxed multi gpu bug

--- deeplab_test.py
+++ deeplab_test.py
@@ -61,7 +61,7 @@ class SimpleDecoderBlock(torch.nn.Module):
 skips = {}
 def get_activation(name):
     def hook(module, input, output):
-        skips[name] = output.detach().to('cuda:0')
+        skips[name] = output.detach()
     return hook
 
 
@@ -82,10 +82,10 @@ class AxialDeeplab(torch.nn.Module):
 
     def forward(self, x):
         x = self.backbone(x)
-        x = self.up1([x, skips[3]])
-        x = self.up2([x, skips[2]])
-        x = self.up3([x, skips[1]])
-        x = self.up4([x, skips[0]])
+        x = self.up1([x, skips[3].to('cuda:0')])
+        x = self.up2([x, skips[2].to('cuda:0')])
+        x = self.up3([x, skips[1].to('cuda:0')])
+        x = self.up4([x, skips[0].to('cuda:0')])
         x = self.up5(x)
         return x
 
