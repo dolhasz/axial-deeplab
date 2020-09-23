20747476b993e6909f51f406b3c41a523cdb1c68
Alan <alan.dolhasz@gmail.com>
2020-09-23 09:58:09 +0100

    Changed dict to list to store activations

--- deeplab_test.py
+++ deeplab_test.py
@@ -58,7 +58,7 @@ class SimpleDecoderBlock(torch.nn.Module):
         return x
 
 
-skips = list(4)
+skips = [None * 4]
 def get_activation(name):
     def hook(module, input, output):
         skips[name] = output.detach()
